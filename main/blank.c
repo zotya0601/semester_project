7fd69ec2deb3ebf6fa011daafadaa358c0dbd962
zotya0601 <zotya0601@gmail.com>
2023-03-14 19:24:50 +0100

    Nothing really changed

--- main/blank.c
+++ main/blank.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "esp_log.h"
+#include "esp_intr_alloc.h"
 
 #include "freertos/FreeRTOS.h"
 
@@ -22,12 +23,12 @@ void i2c_reader(void *params){
 		// https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
 		
 		float measurements_f[3] = {0};
-		read_acc_registers(measurements_f);
+		read_acc_registers_calibrated(measurements_f);
 
-		// for(int i = 0; i < 3; i++){ measurement[i] = get_g_from_measurement(measurement[i]); }
-		ESP_LOGI(I2C_TAG, "Acc X: %f", measurements_f[0]);
-		ESP_LOGI(I2C_TAG, "Acc Y: %f", measurements_f[1]);
-		ESP_LOGI(I2C_TAG, "Acc Z: %f", measurements_f[2]);
+		// for(int i = 0; i < 3; i++){ measurements_f[i] = g_to_acceleration(measurements_f[i]); }
+		ESP_LOGI(I2C_TAG, "Acc X: %f m/s^2", measurements_f[0]);
+		ESP_LOGI(I2C_TAG, "Acc Y: %f m/s^2", measurements_f[1]);
+		ESP_LOGI(I2C_TAG, "Acc Z: %f m/s^2", measurements_f[2]);
 		ESP_LOGI(I2C_TAG, "------------------------------");
 
 		vTaskDelay( xDelay );
@@ -36,6 +37,8 @@ void i2c_reader(void *params){
 
 void app_main(void)
 {
+	est_err_t err = esp_intr_alloc()
+
 	i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = I2C_MASTER_SDA_IO,
@@ -55,6 +58,8 @@ void app_main(void)
 
 	TaskHandle_t i2c_handle = 0;
 	static uint8_t ucParameterToPass;
+
+	
 
 	xTaskCreate(i2c_reader, "Accelerometer loop", 10000, &ucParameterToPass, tskIDLE_PRIORITY, &i2c_handle);
 }
